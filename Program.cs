d14a2188806f81088980230242f9e7d7780685b2
unknown <lvova.nikol@mail.ru>
2020-04-02 22:14:21 +0300

    Создание конструкторов

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace Rectangle
         private double side2 { get; set; }
         private double area { get; set; }
         private double perimeter { get; set; }
+        public Rectangle () {}
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
     }
     class Program
     {
