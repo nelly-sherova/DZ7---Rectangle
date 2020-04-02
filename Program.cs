76cf1a52a05e0fdf8ee31b9b8eb77531ea8499b1
unknown <lvova.nikol@mail.ru>
2020-04-02 22:02:08 +0300

    Объявление класса

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Rectangle
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
